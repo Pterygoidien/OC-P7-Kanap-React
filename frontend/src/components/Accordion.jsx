74d28f440387a8852ab8ec0cdfb2bfa9254e2c09
Bertrand Bouillon <bertrand.bouillon@gmail.com>
2023-03-30 04:29:17 +0200

    fetching urlParam in logements.jsx

--- frontend/src/components/Accordion.jsx
+++ frontend/src/components/Accordion.jsx
@@ -1,7 +1,24 @@
+import styles from './Accordion.module.scss';
 
 const Accordion = ({ data }) => {
     const { title, content } = data;
-    return (<div className="accordion" >accordéon </ div>);
+    return (<article className={styles.accordion}>
+        <div className={styles['accordion__title']}>
+            <h2>{title}</h2>
+        </div>
+        <div className={styles['accordion__content']}>
+            <p>{content}</p>
+        </div>
+    </ article>);
 }
+
+Accordion.defaultProps = {
+    data: {
+        title: "Titre",
+        content: "Contenu"
+    }
+}
+
+
 
 export default Accordion;
--- frontend/src/pages/logement/Logement.jsx
+++ frontend/src/pages/logement/Logement.jsx
@@ -1,6 +1,21 @@
+import { useParams } from "react-router-dom";
 import Accordion from "../../components/Accordion";
 
+import logements from "../../logements.json";
+
+/*
+ url : /logement/:id
+ fetch the id from the url
+*/
+
 const Logement = () => {
+
+
+    const id = useParams();
+    console.log(id);
+
+
+
     return (
         <main className="container">
             Page de logement
